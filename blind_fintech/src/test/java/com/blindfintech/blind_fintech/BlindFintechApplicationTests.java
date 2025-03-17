05922f4c8d3c2a947c66a9d9f09271cce979ac42
uts417923 <uts417923@gmail.com>
2025-03-17 16:57:23 +0900

    initial project 생성

---
+++ blind_fintech/src/test/java/com/blindfintech/blind_fintech/BlindFintechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blindfintech.blind_fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlindFintechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
