f8d22b7cb01a880fa512db2cc8f6f3feda0ed6db
jooo <wndnjs510@naver.com>
2025-03-18 14:40:55 +0900

    style:폴더 구조화

--- blind_fintech/src/main/java/com/blindfintech/BlindFintechApplication.java
+++ blind_fintech/src/main/java/com/blindfintech/BlindFintechApplication.java
@@ -1,4 +1,4 @@
-package com.blindfintech.blind_fintech;
+package com.blindfintech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- blind_fintech/src/test/java/com/blindfintech/BlindFintechApplicationTests.java
+++ blind_fintech/src/test/java/com/blindfintech/BlindFintechApplicationTests.java
@@ -1,4 +1,4 @@
-package com.blindfintech.blind_fintech;
+package com.blindfintech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
