ee5042189e7e070b0f4dea2737595f6cf461888b
uts417923 <uts417923@gmail.com>
2025-03-19 15:31:50 +0900

    chore: Exception 세팅

---
+++ blind_fintech/src/main/java/com/blindfintech/common/exception/TokenException.java
@@ -0,0 +1,12 @@
+package com.blindfintech.common.exception;
+
+public class TokenException extends BaseException {
+
+    public TokenException(ExceptionCode exceptionCode) {
+        super(exceptionCode);
+    }
+
+    public TokenException(String message) {
+        super("INVALID_TOKEN", message);
+    }
+}
