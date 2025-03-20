a49da5f602bd964bc8ea188bc6846f4eb3339444
조성빈 <tjdqls420@naver.com>
2025-03-20 19:00:08 +0900

    feat: accounts check
    자신의 계좌목록 조회
    자신의 User 객체를 가져오는 getCurrentUser 기본 메서드 작성

---
+++ blind_fintech/src/main/java/com/blindfintech/domain/accounts/controller/AccountController.java
@@ -0,0 +1,26 @@
+package com.blindfintech.domain.accounts.controller;
+
+import com.blindfintech.domain.accounts.dto.AccountListDto;
+import com.blindfintech.domain.accounts.service.AccountService;
+import com.blindfintech.domain.users.entity.User;
+import com.blindfintech.domain.users.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+    private final UserService userService;
+
+    @GetMapping()
+    public ResponseEntity<?> getAccounts() {
+        User user = userService.getCurrentUser();
+        AccountListDto accounts = accountService.getAccounts(user);
+        return ResponseEntity.ok(accounts);
+    }
+}
--- blind_fintech/src/main/java/com/blindfintech/domain/accounts/repository/AccountRepository.java
+++ blind_fintech/src/main/java/com/blindfintech/domain/accounts/repository/AccountRepository.java
@@ -1,4 +1,20 @@
 package com.blindfintech.domain.accounts.repository;
 
-public class AccountRepository {
+import com.blindfintech.domain.accounts.entity.Account;
+import com.blindfintech.domain.users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    @Query("""
+        SELECT a.id, a.bankId, a.accountNo, a.username, a.accountBalance,
+        a.dailyTransferLimit, a.oneTimeTransferLimit, a.createdAt,
+        a.failedAttempts, a.accountState
+        FROM Account a
+        WHERE a.user = :user
+    """)
+    List<Account> findAllByUser(@Param("user") User user);
 }
