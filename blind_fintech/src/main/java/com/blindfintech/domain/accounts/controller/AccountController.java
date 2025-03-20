e3a5586739f21139106a42a33fa28c021c2f4aae
조성빈 <tjdqls420@naver.com>
2025-03-20 19:11:20 +0900

    refactor: use ResponseDto
    반환값 ResponseDto 사용하여 반환

--- blind_fintech/src/main/java/com/blindfintech/domain/accounts/controller/AccountController.java
+++ blind_fintech/src/main/java/com/blindfintech/domain/accounts/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.blindfintech.domain.accounts.controller;
 
+import com.blindfintech.common.dto.ResponseDto;
 import com.blindfintech.domain.accounts.dto.AccountListDto;
 import com.blindfintech.domain.accounts.service.AccountService;
 import com.blindfintech.domain.users.entity.User;
@@ -21,6 +22,7 @@ public class AccountController {
     public ResponseEntity<?> getAccounts() {
         User user = userService.getCurrentUser();
         AccountListDto accounts = accountService.getAccounts(user);
-        return ResponseEntity.ok(accounts);
+        return ResponseEntity.ok()
+                .body(ResponseDto.success(accounts));
     }
 }
