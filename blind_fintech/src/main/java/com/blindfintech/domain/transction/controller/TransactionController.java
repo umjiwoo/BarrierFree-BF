f280e576ae67a0e75a31fd23bb4a9a21946a8f8f
uts417923 <uts417923@gmail.com>
2025-03-28 10:35:42 +0900

    refactor: CONTEXT_PATH 설정 추가에 따른 경로 수정
    Related to: S12P21A208-27

--- blind_fintech/src/main/java/com/blindfintech/domain/transction/controller/TransactionController.java
+++ blind_fintech/src/main/java/com/blindfintech/domain/transction/controller/TransactionController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/accounts/transaction")
+@RequestMapping("/accounts/transaction")
 public class TransactionController {
     private final TransactionService transactionService;
 
