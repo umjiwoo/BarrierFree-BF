338c5e20ee6cbf6c19687bd84c2c5d5657d1f71f
uts417923 <uts417923@gmail.com>
2025-04-03 17:32:24 +0900

    feat: 최근 거래 계좌 내역 조회
    S12P21A208-28

--- blind_fintech/src/main/java/com/blindfintech/domain/transction/controller/TransactionController.java
+++ blind_fintech/src/main/java/com/blindfintech/domain/transction/controller/TransactionController.java
@@ -4,7 +4,7 @@ import com.blindfintech.common.dto.ResponseDto;
 import com.blindfintech.domain.transction.controller.request.CheckAccountRequest;
 import com.blindfintech.domain.transction.controller.request.TransactionRequest;
 import com.blindfintech.domain.transction.dto.CheckAccountResultDto;
-import com.blindfintech.domain.transction.dto.TransactionResultDto;
+import com.blindfintech.domain.transction.dto.RecentTransactionAccountDto;
 import com.blindfintech.domain.transction.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/transaction")
+@RequestMapping("/transactions")
 public class TransactionController {
     private final TransactionService transactionService;
 
@@ -32,5 +34,11 @@ public class TransactionController {
 
         transactionService.produceSendMoney(transactionRequest);
         return ResponseEntity.ok().body("ok");
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<ResponseDto<List<RecentTransactionAccountDto>>> getTransactionHistory(){
+        List<RecentTransactionAccountDto> recentTransactionAccounts = transactionService.getRecentTransactionAccounts();
+        return ResponseEntity.ok(ResponseDto.success(recentTransactionAccounts));
     }
 }
