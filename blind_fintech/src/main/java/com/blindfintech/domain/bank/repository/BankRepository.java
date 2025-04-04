133fd605c4aaf7c2e3704f5d6a7942b32a97b1ea
uts417923 <uts417923@gmail.com>
2025-04-04 14:48:21 +0900

    feat: 은행 조회

--- blind_fintech/src/main/java/com/blindfintech/domain/bank/repository/BankRepository.java
+++ blind_fintech/src/main/java/com/blindfintech/domain/bank/repository/BankRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BankRepository extends JpaRepository<Bank, Long> {
-
+    public Bank findBankById(Integer bankId);
 }
