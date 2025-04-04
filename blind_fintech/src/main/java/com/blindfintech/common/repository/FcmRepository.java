ef545b3c034af4e50d0b7eeda636947e10097d06
jooo <wndnjs510@naver.com>
2025-04-04 14:34:09 +0900

    add fcmRepo

---
+++ blind_fintech/src/main/java/com/blindfintech/common/repository/FcmRepository.java
@@ -0,0 +1,12 @@
+package com.blindfintech.common.repository;
+
+import com.blindfintech.common.dto.FcmDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class FcmRepository {
+    public void save(FcmDto fcmDto) {
+    }
+}
--- blind_fintech/src/main/java/com/blindfintech/common/service/FcmService.java
+++ blind_fintech/src/main/java/com/blindfintech/common/service/FcmService.java
@@ -1,6 +1,7 @@
 package com.blindfintech.common.service;
 
 import com.blindfintech.common.dto.FcmDto;
+import com.blindfintech.common.repository.FcmRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class FcmService {
-
+    private final FcmRepository fcmRepository;
 
     public void saveFcmToken(FcmDto fcmDto) {
-
+        fcmRepository.save(fcmDto);
     }
 }
