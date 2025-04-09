586076b25c8825af7033ba63c008a15b4873aaa6
uts417923 <uts417923@gmail.com>
2025-04-09 15:40:01 +0900

    feat: WRONG_PASSWORD 예외 코드 추가
    송금 및 결제 승인 api 수행 시 비밀번호 검증 부분 예외 응답 추가

--- blind_fintech/src/main/java/com/blindfintech/domain/accounts/exception/AccountExceptionCode.java
+++ blind_fintech/src/main/java/com/blindfintech/domain/accounts/exception/AccountExceptionCode.java
@@ -9,8 +9,9 @@ import lombok.AllArgsConstructor;
 public enum AccountExceptionCode implements ExceptionCode {
     ACCOUNT_ALREADY_EXISTS(2001, "이미 존재하는 계좌입니다."),
     PASSWORD_ERROR(2002, "비밀번호는 숫자 4자리를 입력해야합니다."),
-    ONETIME_LIMIT_OVER(2003, "1회 이체 한도는 1일 이체 한도를 초과할 수 없습니다.");
-    
+    ONETIME_LIMIT_OVER(2003, "1회 이체 한도는 1일 이체 한도를 초과할 수 없습니다."),
+    WRONG_PASSWORD(2004, "비밀번호가 틀렸습니다.");
+
     private final int code;
     private final String message;
 }
