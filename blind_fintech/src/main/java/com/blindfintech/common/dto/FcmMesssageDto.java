2bdfecdbaa00aa637b107a6a2070d3098b313cd3
uts417923 <uts417923@gmail.com>
2025-04-09 15:43:32 +0900

    feat: FCM 토큰 저장
    로그인 시(앱 첫 화면 진입 시) FCM 토큰 발급받아 저장
    
    S12P21A208-96

--- blind_fintech/src/main/java/com/blindfintech/common/dto/FcmMesssageDto.java
+++ blind_fintech/src/main/java/com/blindfintech/common/dto/FcmMesssageDto.java
@@ -2,14 +2,16 @@ package com.blindfintech.common.dto;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class FcmMesssageDto {
-    private Integer userId;
+    private Long userId;
     private String title;
     private String message;
 }
